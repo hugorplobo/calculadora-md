6cb2f9dc45ebd6251eaa0a9b43e7a811c12157da
Antônio Hugo <hugorplobo@gmail.com>
2021-10-29 13:35:54 -0300

    Update conlusions page

--- src/components/conclusions/conclusions.jsx
+++ src/components/conclusions/conclusions.jsx
@@ -2,12 +2,12 @@ import { ContentPage } from "../contentPage/contentPage";
 
 export function Conclusions() {
     const labels = [
-        {label: "As conclusões necessárias foram alcançadas corretamente", value: 0.1},
-        {label: "As conclusões necessárias foram alcançadas corretamente", value: 0.05},
-        {label: " As conclusões necessárias não foram alcançadas ou foram mencionadas sem conexão com a argumentação", value: 0}
+        {label: "As conclusões necessárias foram alcançadas corretamente", value: 0.1, alternative: "a"},
+        {label: "As conclusões necessárias foram alcançadas corretamente", value: 0.05, alternative: "b"},
+        {label: " As conclusões necessárias não foram alcançadas ou foram mencionadas sem conexão com a argumentação", value: 0, alternative: "c"}
     ];
 
     return (
-        <ContentPage name="conclusions" title="Conclusões alcançadas" description="Este critério visa, levando em conta as técnicas usadas na argumentação, se as conclusões a que se desejava chegar foram corretamente alcançadas." sentences={labels}></ContentPage>
+        <ContentPage urlNext="/calculate" urlPrevious="/validArguments" hasNext={true} hasPrevious={true} name="conclusions" title="Conclusões alcançadas" description="Este critério visa, levando em conta as técnicas usadas na argumentação, se as conclusões a que se desejava chegar foram corretamente alcançadas." sentences={labels}></ContentPage>
     )
 }
