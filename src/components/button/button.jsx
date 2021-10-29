93c44212971e76916cecb9afc80941720a133852
Antônio Hugo <hugorplobo@gmail.com>
2021-10-29 13:35:23 -0300

    Update button component

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,7 +1,7 @@
 import "./button.css";
 
-export function Button({ text, url }) {
+export function Button({ text, url, style }) {
     return (
-        <a className="button" href={url}>{text}</a>
+        <a className="button" href={url} style={style}>{text}</a>
     )
 }
